0a73c82ec2bdef903df6bbac1910fd2c392acec8
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-01-29 12:07:23 +0100

    Profile and supplier association

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :city
+  
   has_one :profile, dependent: :destroy
-  has_one :land, dependent: :destroy
-  has_one :houses, dependent: :destroy
+  has_one :supplier, dependent: :destroy
 
   validates_presence_of :street, :city
 end
